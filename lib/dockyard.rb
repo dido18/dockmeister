3285e5d008a647a4d48b2ab67e9a7b5a44308e97
Bruno Abrantes <bruno@brunoabrantes.com>
2015-06-08 17:05:43 +0200

    Adds error handling for loading the config file

--- lib/dockyard.rb
+++ lib/dockyard.rb
@@ -2,5 +2,21 @@ require_relative "dockyard/version"
 require_relative "dockyard/composer"
 
 module Dockyard
+  DOCKYARD_CONFIGURATION_FILE = 'dockyard.yml'
 
+  def self.load_config
+    unless File.exists?('dockyard.yml')
+      puts "Missing dockyard.yml configuration file"
+      exit 0
+    end
+
+    config = YAML.load_file(DOCKYARD_CONFIGURATION_FILE)
+
+    unless config
+      puts "Invalid dockyard.yml configuration file"
+      exit 0
+    end
+
+    config
+  end
 end
