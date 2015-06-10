8eb834c5de087d6ed2ccdc57bbbcecd069f0bd20
Laurens Nienhaus <lnienhaus@babbel.com>
2015-06-10 09:37:56 +0200

    Refactor base_path

--- lib/dockyard/script_runner.rb
+++ lib/dockyard/script_runner.rb
@@ -1,22 +1,23 @@
 module Dockyard
   class ScriptRunner
     def initialize(base_path)
-      @base_path = base_path
+      @base_path = File.expand_path(base_path)
     end
 
     def run_script(script)
       success = system(script)
 
       unless success
-        STDERR.puts ""
-        STDERR.puts "A dockyard init script failed."
+        STDERR.puts <<-eos
+
+          A dockyard init script failed.
+        eos
         exit(1)
       end
     end
 
     def pre_build!
       Dir.chdir(@base_path)
-      base_dir = Dir.pwd
 
       Dockyard.load_config(@base_path)['services'].each do |service_name|
         Dir.chdir(service_name)
@@ -25,7 +26,7 @@ module Dockyard
           success = run_script(script)
         end
 
-        Dir.chdir(base_dir)
+        Dir.chdir(@base_path)
       end
     end
   end
